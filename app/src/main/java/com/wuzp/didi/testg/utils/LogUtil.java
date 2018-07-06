c4f77e15c93af6c4d7dc3314b614a57eef0aa297
wuzhenpeng03 <wuzhenpengh03@didichuxing.com>
2018-07-06 12:03:41 +0800

    [b]master fix:update 3 description: modify by:wuzhenpeng03

--- app/src/main/java/com/wuzp/didi/testg/utils/LogUtil.java
+++ app/src/main/java/com/wuzp/didi/testg/utils/LogUtil.java
@@ -11,4 +11,10 @@ public class LogUtil {
     public static void e(String tag, String msg) {
         Log.e(tag, msg);
     }
+
+    public static void i(String tag, String msg) {
+        if (DEBUG) {
+            Log.i(tag, msg);
+        }
+    }
 }
