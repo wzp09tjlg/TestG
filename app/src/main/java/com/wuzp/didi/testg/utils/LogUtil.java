2925c39a1da3e332dd505798df12f6cbfa6d392a
wuzhenpeng03 <wuzhenpengh03@didichuxing.com>
2018-07-06 11:56:40 +0800

    [bz]master fix:update2 reset --soft description: modify by:wuzhenpeng03

--- app/src/main/java/com/wuzp/didi/testg/utils/LogUtil.java
+++ app/src/main/java/com/wuzp/didi/testg/utils/LogUtil.java
@@ -1,10 +1,14 @@
 package com.wuzp.didi.testg.utils;
 
+import android.util.Log;
+
 /**
  * @author wuzhenpeng03 (wuzhenpeng03@didichuxing.com)
  */
 public class LogUtil {
     public static boolean DEBUG = true;
 
-
+    public static void e(String tag, String msg) {
+        Log.e(tag, msg);
+    }
 }
